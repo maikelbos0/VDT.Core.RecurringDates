3c879376013bc5b15908f37e86579b148fb2e08f
Maikel Bos <maikel.bos0@gmail.com>
2023-03-18 09:59:54 +0100

    Further standardize test coverage

--- src/VDT.Core.RecurringDates.Tests/RecurrenceTests.cs
+++ src/VDT.Core.RecurringDates.Tests/RecurrenceTests.cs
@@ -73,18 +73,6 @@ namespace VDT.Core.RecurringDates.Tests {
         }
 
         [Fact]
-        public void GetDates_Offset_ReferenceDate() {
-            var recurrence = new Recurrence(new DateTime(2022, 1, 1), new DateTime(2022, 1, 4), null, new[] { new DailyRecurrencePattern(2, new DateTime(2022, 1, 2)) }, false);
-
-            var dates = recurrence.GetDates();
-
-            Assert.Equal(new[] {
-                new DateTime(2022, 1, 2),
-                new DateTime(2022, 1, 4)
-            }, dates);
-        }
-
-        [Fact]
         public void GetDates_Occurrences() {
             var recurrence = new Recurrence(new DateTime(2022, 1, 1), DateTime.MaxValue, 2, new[] { new DailyRecurrencePattern(2, new DateTime(2022, 1, 1)) }, false);
 
@@ -110,8 +98,9 @@ namespace VDT.Core.RecurringDates.Tests {
 
         [Theory]
         [InlineData("2022-01-01")]
+        [InlineData("2022-01-02")]
+        [InlineData("2022-01-03")]
         [InlineData("2022-01-04")]
-        [InlineData("2022-01-06")]
         public void IsValidInAnyPattern_No_Pattern(DateTime date) {
             var recurrence = new Recurrence(new DateTime(2022, 1, 1), new DateTime(2022, 1, 11), null, Enumerable.Empty<RecurrencePattern>(), false);
 
@@ -123,10 +112,19 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData("2022-01-02", false)]
         [InlineData("2022-01-03", true)]
         [InlineData("2022-01-04", false)]
-        [InlineData("2022-01-05", true)]
-        [InlineData("2022-01-06", false)]
         public void IsValidInAnyPattern_Single_Pattern(DateTime date, bool expectedIsValid) {
             var recurrence = new Recurrence(DateTime.MinValue, DateTime.MaxValue, null, new[] { new DailyRecurrencePattern(2, new DateTime(2022, 1, 1)) }, false);
+
+            Assert.Equal(expectedIsValid, recurrence.IsValidInAnyPattern(date));
+        }
+
+        [Theory]
+        [InlineData("2022-01-01", false)]
+        [InlineData("2022-01-02", true)]
+        [InlineData("2022-01-03", false)]
+        [InlineData("2022-01-04", true)]
+        public void IsValidInAnyPattern_Offset_ReferenceDate(DateTime date, bool expectedIsValid) {
+            var recurrence = new Recurrence(DateTime.MinValue, DateTime.MaxValue, null, new[] { new DailyRecurrencePattern(2, new DateTime(2022, 1, 2)) }, false);
 
             Assert.Equal(expectedIsValid, recurrence.IsValidInAnyPattern(date));
         }
@@ -136,8 +134,6 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData("2022-01-02", false)]
         [InlineData("2022-01-03", true)]
         [InlineData("2022-01-04", true)]
-        [InlineData("2022-01-05", true)]
-        [InlineData("2022-01-06", false)]
         public void IsValidInAnyPattern_Double_Pattern(DateTime date, bool expectedIsValid) {
             var recurrence = new Recurrence(DateTime.MinValue, DateTime.MaxValue, null, new[] { new DailyRecurrencePattern(2, new DateTime(2022, 1, 1)), new DailyRecurrencePattern(3, new DateTime(2022, 1, 1)) }, false);
 
