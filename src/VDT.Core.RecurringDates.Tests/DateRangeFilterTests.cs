8e5ef0fa472c83a4ca5095f7f669cca4f38ee629
Maikel Bos <maikel.bos0@gmail.com>
2023-03-19 08:55:43 +0100

    Add filter for ranges

---
+++ src/VDT.Core.RecurringDates.Tests/DateRangeFilterTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace VDT.Core.RecurringDates.Tests {
+    public class DateRangeFilterTests {
+        [Fact]
+        public void Constructor_Without_StartDate_Sets_DateTime_MinValue() {
+            var filter = new DateRangeFilter(null, new DateTime(2022, 1, 11));
+
+            Assert.Equal(DateTime.MinValue, filter.StartDate);
+        }
+
+        [Fact]
+        public void Constructor_Without_EndDate_Sets_DateTime_MaxValue() {
+            var filter = new DateRangeFilter(new DateTime(2022, 1, 1), null);
+
+            Assert.Equal(DateTime.MaxValue, filter.EndDate);
+        }
+
+        [Theory]
+        [InlineData("2022-01-01", false)]
+        [InlineData("2022-01-02", true)]
+        [InlineData("2022-01-03", true)]
+        [InlineData("2022-01-04", true)]
+        [InlineData("2022-01-05", false)]
+        public void IsFiltered(DateTime date, bool expectedIsFiltered) {
+            var filter = new DateRangeFilter(new DateTime(2022, 1, 2), new DateTime(2022, 1, 4));
+
+            Assert.Equal(expectedIsFiltered, filter.IsFiltered(date));
+        }
+    }
+}
---
+++ src/VDT.Core.RecurringDates/DateRangeFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VDT.Core.RecurringDates {
+    /// <summary>
+    /// Filter to invalidate date ranges that would otherwise be valid
+    /// </summary>
+    public class DateRangeFilter : IFilter {
+        /// <summary>
+        /// Gets the inclusive date from which dates will be invalidated
+        /// </summary>
+        public DateTime StartDate { get; }
+
+        /// <summary>
+        /// Gets the inclusive date up to which dates will be invalidated
+        /// </summary>
+        public DateTime EndDate { get; }
+
+        /// <summary>
+        /// Create a filter to invalidate date ranges
+        /// </summary>
+        /// <param name="startDate">Inclusive date from which dates will be invalidated</param>
+        /// <param name="endDate">Inclusive date up to which dates will be invalidated</param>
+        public DateRangeFilter(DateTime? startDate, DateTime? endDate) {
+            StartDate = startDate?.Date ?? DateTime.MinValue;
+            EndDate = endDate?.Date ?? DateTime.MaxValue;
+        }
+
+        /// <inheritdoc/>
+        public bool IsFiltered(DateTime date) => date >= StartDate && date <= EndDate;
+    }
+}
