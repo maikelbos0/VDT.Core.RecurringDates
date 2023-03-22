613c073372a5495245e0206985effe6bdf45f3c4
Maikel Bos <maikel.bos0@gmail.com>
2023-03-22 17:45:05 +0100

    Switch guard tests to fluent assertions

--- src/VDT.Core.RecurringDates.Tests/GuardTests.cs
+++ src/VDT.Core.RecurringDates.Tests/GuardTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using Xunit;
 
 namespace VDT.Core.RecurringDates.Tests {
@@ -8,9 +9,7 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(2)]
         [InlineData(int.MaxValue)]
         public void IsPositive_Returns_Positive_Numbers(int value) {
-            var result = Guard.IsPositive(value);
-
-            Assert.Equal(result, value);
+            Guard.IsPositive(value).Should().Be(value);
         }
 
         [Theory]
@@ -18,7 +17,9 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(-1)]
         [InlineData(int.MinValue)]
         public void IsPositive_Throws_For_Negative_Numbers_Or_Zero(int value) {
-            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.IsPositive(value));
+            var action = () => Guard.IsPositive(value);
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
 
         [Theory]
@@ -26,9 +27,7 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(2, 9, 19)]
         [InlineData(int.MaxValue)]
         public void ArePositive_Returns_Positive_Numbers(params int[] values) {
-            var result = Guard.ArePositive(values);
-
-            Assert.Equal(result, values);
+            Guard.ArePositive(values).Should().BeEquivalentTo(values);
         }
 
         [Theory]
@@ -36,7 +35,9 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(9, 19, -1)]
         [InlineData(int.MinValue)]
         public void ArePositive_Throws_For_Negative_Numbers_Or_Zero(params int[] values) {
-            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.ArePositive(values));
+            var action = () => Guard.ArePositive(values);
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
     }
 }
