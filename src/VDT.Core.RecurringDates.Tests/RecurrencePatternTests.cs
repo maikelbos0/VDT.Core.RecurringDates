5b217a939d7e2ebc6e2600addc4d1edd4d5a6139
Maikel Bos <maikel.bos0@gmail.com>
2023-03-25 08:10:01 +0100

    Switch recurrence pattern tests to fluent assertions

--- src/VDT.Core.RecurringDates.Tests/RecurrencePatternTests.cs
+++ src/VDT.Core.RecurringDates.Tests/RecurrencePatternTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using Xunit;
 
 namespace VDT.Core.RecurringDates.Tests {
@@ -14,21 +15,23 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(-1)]
         [InlineData(int.MinValue)]
         public void Constructor_Throws_For_Invalid_Interval(int interval) {
-            Assert.Throws<ArgumentOutOfRangeException>(() => new TestRecurrencePattern(interval, DateTime.MinValue));
+            var action = () => new TestRecurrencePattern(interval, DateTime.MinValue);
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
 
         [Fact]
         public void Constructor_Without_ReferenceDate_Sets_DateTime_MinValue() {
             var pattern = new TestRecurrencePattern(1, null);
 
-            Assert.Equal(DateTime.MinValue.Date, pattern.ReferenceDate);
+            pattern.ReferenceDate.Should().Be(DateTime.MinValue.Date);
         }
 
         [Fact]
         public void Constructor_Removes_Time_From_ReferenceDate() {
             var pattern = new TestRecurrencePattern(1, new DateTime(2022, 1, 2, 11, 12, 30));
 
-            Assert.Equal(new DateTime(2022, 1, 2), pattern.ReferenceDate);
+            pattern.ReferenceDate.Should().Be(new DateTime(2022, 1, 2));
         }
     }
 }
