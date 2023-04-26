2aa52365725a1ff10d6c5a3946662c3e30e3c4d5
Maikel Bos <maikel.bos0@gmail.com>
2023-04-26 19:34:12 +0200

    Add filter builder for intersecting recurrence

---
+++ src/VDT.Core.RecurringDates.Tests/RecurrenceFilterBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace VDT.Core.RecurringDates.Tests {
+    public class RecurrenceFilterBuilderTests {
+        [Fact]
+        public void Intersecting() {
+            var recurrence = new Recurrence(null, null, null, Enumerable.Empty<RecurrencePattern>(), Enumerable.Empty<IFilter>(), false);
+            var builder = new RecurrenceFilterBuilder(new RecurrenceBuilder(), new Recurrence(null, null, null, Enumerable.Empty<RecurrencePattern>(), Enumerable.Empty<IFilter>(), false));
+
+            builder.Should().BeSameAs(builder.Intersecting(recurrence));
+
+            builder.Recurrence.Should().BeSameAs(recurrence);
+        }
+
+        [Fact]
+        public void BuildFilter() {
+            var recurrence = new Recurrence(null, null, null, Enumerable.Empty<RecurrencePattern>(), Enumerable.Empty<IFilter>(), false);
+            var builder = new RecurrenceFilterBuilder(new RecurrenceBuilder(), recurrence);
+
+            builder.BuildFilter().Should().BeOfType<RecurrenceFilter>().Which.Recurrence.Should().BeSameAs(recurrence);
+        }
+    }
+}
