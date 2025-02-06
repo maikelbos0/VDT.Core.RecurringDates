eb8f129d9410e40807f2ed0a77ac445c21986994
Maikel Bos <maikel.bos0@gmail.com>
2025-02-06 20:01:03 +0100

    Remove FluentAssertions dependency for builder startging point

--- src/VDT.Core.RecurringDates.Tests/RecursTests.cs
+++ src/VDT.Core.RecurringDates.Tests/RecursTests.cs
@@ -1,5 +1,5 @@
-﻿using FluentAssertions;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace VDT.Core.RecurringDates.Tests {
@@ -8,86 +8,85 @@ namespace VDT.Core.RecurringDates.Tests {
         public void From() {
             var result = Recurs.From(new DateTime(2022, 1, 1));
 
-            result.Should().BeOfType<RecurrenceBuilder>().Which.StartDate.Should().Be(new DateTime(2022, 1, 1));
+            Assert.Equal(new DateTime(2022, 1, 1), Assert.IsType<RecurrenceBuilder>(result).StartDate);
         }
 
         [Fact]
         public void Until() {
             var result = Recurs.Until(new DateTime(2022, 12, 31));
 
-            result.Should().BeOfType<RecurrenceBuilder>().Which.EndDate.Should().Be(new DateTime(2022, 12, 31));
+            Assert.Equal(new DateTime(2022, 12, 31), Assert.IsType<RecurrenceBuilder>(result).EndDate);
         }
 
         [Fact]
         public void StopAfter() {
             var result = Recurs.StopAfter(10);
 
-            result.Should().BeOfType<RecurrenceBuilder>().Which.Occurrences.Should().Be(10);
+            Assert.Equal(10, Assert.IsType<RecurrenceBuilder>(result).Occurrences);
         }
 
         [Fact]
         public void Daily() {
             var result = Recurs.Daily();
 
-            result.Interval.Should().Be(1);
+            Assert.Equal(1, result.Interval);
         }
 
         [Fact]
         public void Weekly() {
             var result = Recurs.Weekly();
 
-            result.Interval.Should().Be(1);
+            Assert.Equal(1, result.Interval);
         }
 
         [Fact]
         public void Monthly() {
             var result = Recurs.Monthly();
 
-            result.Interval.Should().Be(1);
+            Assert.Equal(1, result.Interval);
         }
 
         [Fact]
         public void Every() {
             var result = Recurs.Every(2);
 
-            result.Interval.Should().Be(2);
+            Assert.Equal(2, result.Interval);
         }
 
         [Fact]
         public void WithDateCaching() {
             var result = Recurs.WithDateCaching();
 
-            result.CacheDates.Should().BeTrue();
+            Assert.True(result.CacheDates);
         }
-
 
         [Fact]
         public void ExceptOn() {
             var result = Recurs.ExceptOn(new DateTime(2022, 1, 1), new DateTime(2022, 1, 2));
 
-            result.Dates.Should().Equal(new DateTime(2022, 1, 1), new DateTime(2022, 1, 2));
+            Assert.Equivalent(new List<DateTime>() { new DateTime(2022, 1, 1), new DateTime(2022, 1, 2) }, result.Dates);
         }
 
         [Fact]
         public void ExceptFrom() {
             var result = Recurs.ExceptFrom(new DateTime(2022, 2, 3));
 
-            result.StartDate.Should().Be(new DateTime(2022, 2, 3));
+            Assert.Equal(new DateTime(2022, 2, 3), result.StartDate);
         }
 
         [Fact]
         public void ExceptUntil() {
             var result = Recurs.ExceptUntil(new DateTime(2022, 2, 5));
 
-            result.EndDate.Should().Be(new DateTime(2022, 2, 5));
+            Assert.Equal(new DateTime(2022, 2, 5), result.EndDate);
         }
 
         [Fact]
         public void ExceptBetween() {
             var result = Recurs.ExceptBetween(new DateTime(2022, 2, 3), new DateTime(2022, 2, 5));
 
-            result.StartDate.Should().Be(new DateTime(2022, 2, 3));
-            result.EndDate.Should().Be(new DateTime(2022, 2, 5));
+            Assert.Equal(new DateTime(2022, 2, 3), result.StartDate);
+            Assert.Equal(new DateTime(2022, 2, 5), result.EndDate);
         }
     }
 }
