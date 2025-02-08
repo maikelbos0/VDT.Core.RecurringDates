ffff3512526daffabc378303aada906986c0a499
Maikel Bos <maikel.bos0@gmail.com>
2025-02-08 12:56:27 +0100

    Add DateOnly overloads to date filter building

--- src/VDT.Core.RecurringDates.Tests/DateFilterBuilderTests.cs
+++ src/VDT.Core.RecurringDates.Tests/DateFilterBuilderTests.cs
@@ -1,11 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace VDT.Core.RecurringDates.Tests;
 
 public class DateFilterBuilderTests {
     [Fact]
-    public void On() {
+    public void On_Array_DateOnly() {
+        var builder = new DateFilterBuilder(new RecurrenceBuilder()) {
+            Dates = [
+                new DateTime(2022, 2, 1),
+                new DateTime(2022, 2, 3),
+                new DateTime(2022, 2, 5)
+            ]
+        };
+
+        Assert.Same(builder, builder.On(new DateOnly(2022, 2, 2), new DateOnly(2022, 2, 4)));
+
+        Assert.Equal([
+            new DateTime(2022, 2, 1),
+            new DateTime(2022, 2, 3),
+            new DateTime(2022, 2, 5),
+            new DateTime(2022, 2, 2),
+            new DateTime(2022, 2, 4)
+        ], builder.Dates);
+    }
+
+    [Fact]
+    public void On_IEnumerable_DateOnly() {
+        var builder = new DateFilterBuilder(new RecurrenceBuilder()) {
+            Dates = [
+                new DateTime(2022, 2, 1),
+                new DateTime(2022, 2, 3),
+                new DateTime(2022, 2, 5)
+            ]
+        };
+
+        Assert.Same(builder, builder.On(new List<DateOnly>() { new(2022, 2, 2), new(2022, 2, 4) }));
+
+        Assert.Equal([
+            new DateTime(2022, 2, 1),
+            new DateTime(2022, 2, 3),
+            new DateTime(2022, 2, 5),
+            new DateTime(2022, 2, 2),
+            new DateTime(2022, 2, 4)
+        ], builder.Dates);
+    }
+
+    [Fact]
+    public void On_Array_DateTime() {
         var builder = new DateFilterBuilder(new RecurrenceBuilder()) {
             Dates = [
                 new DateTime(2022, 2, 1),
